d92ad88dc586d45ba96c99e22656ce5c7925adb5
Ioan Vancea <ionut@vioan.ro>
2016-10-10 19:03:04 +0200

    Added the config files and a minimal Flask app

---
+++ flaskapp/app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello from FLASK"
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1')
